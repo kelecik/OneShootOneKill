15d689f3725f1ac2486219b5a425a43683865544
Kelecik <sinanince484@gmail.com>
2021-03-09 21:02:32 +0300

    Initial commit

---
+++ Assets/GameFolder/Scripts/Soldier/Soldier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Soldier : MonoBehaviour
+{
+    private SoldierState soldierState = SoldierState.NONE;
+    [SerializeField]
+    SoldierMovementAI soldierMovement;
+    [SerializeField]
+    SoldierAttackAI soldierAttackAI;
+    [SerializeField]
+    SoldierAnimation soldierAnimation;
+    [SerializeField]
+    SoldierDeterminationAI soldierDeterminationAI;
+}
+
---
+++ Assets/GameFolder/Scripts/Soldier/SoldierDeterminationAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierDeterminationAI : MonoBehaviour
+{
+    [SerializeField]
+    Collider[] nearstObjectContainer;
+    [SerializeField]
+    LayerMask nearsObjectLayer;
+
+
+    private void Update()
+    {
+        Determination();
+    }
+    void Determination()
+    {
+        nearstObjectContainer = Physics.OverlapSphere(transform.position,10,nearsObjectLayer,QueryTriggerInteraction.UseGlobal);
+    }
+}
