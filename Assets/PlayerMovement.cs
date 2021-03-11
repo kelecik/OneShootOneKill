d6bb93877318c9755e51f3afbcccb32446299ba8
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-11 11:05:20 +0300

    PlayerMovement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public List<GameObject> siperler = new List<GameObject>();
+    private Transform hedef;
+    public bool ısAlive = true;
+    private int siper;
+    private void Start()
+    {
+        siper = Random.Range(0, siperler.Count);
+        Transform pos = siperler[siper].transform;
+        hedef = pos;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Debug.Log("Yerlerini, değiştir");
+            hedef.position = siperler[(Random.Range(0, siperler.Count)) % siperler.Count].transform.position;
+        }
+        if (ısAlive)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(hedef.position.x, transform.position.y, hedef.position.z), 15 * Time.deltaTime);
+        }
+        else if (ısAlive == false)
+        {
+
+        }
+    }
+}
