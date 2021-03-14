c2227b68c055b1412e0c8cb7af7a78f2723d8515
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2021-03-14 13:16:43 +0300

    ScriptableObjects-Enemydata-Playerdata
    icleri doldurulacak orasini size biraktim canlarim <3

---
+++ Assets/GameFolder/Scripts/Soldier/SoldierInfoAsset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "SoldierAsset", menuName = "Soldier", order = 1)]
+public class SoldierInfoAsset : ScriptableObject
+{
+    public GameObject BulletPrefab;
+    public int FireFriquency;
+    public Transform[] TrencList;
+    public int LayerCount;
+}
